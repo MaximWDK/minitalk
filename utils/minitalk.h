3611ba708bfbdcc4eeefdcb3f43c4f0c9d5e2caa
mleonet <mleonet@student.s19.be>
2023-08-16 12:00:36 +0200

    Fixed libs

--- utils/minitalk.h
+++ utils/minitalk.h
@@ -6,14 +6,13 @@
 /*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 22:53:49 by mleonet           #+#    #+#             */
-/*   Updated: 2023/08/15 23:11:26 by mleonet          ###   ########.fr       */
+/*   Updated: 2023/08/16 12:00:00 by mleonet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include <sys/types.h>
 # include <signal.h>
 # include "../ft_printf/ft_printf.h"
 
